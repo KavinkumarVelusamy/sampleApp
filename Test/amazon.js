b67a7e77eb86ba50785b4fbcc70af9a5c4f697c7
Kavinkumar Velusamy <kavinkumar.velusamy@neartekpod.com>
2024-08-01 19:36:56 +0530

    chose the product

--- Test/amazon.js
+++ Test/amazon.js
@@ -1,10 +1,15 @@
 module.exports={
-        'jacana':function(browser){
+        'Test1':function(browser){
             browser.url("https://www.amazon.in")
             .maximizeWindow()
             .pause(3000)
             .setValue('#twotabsearchtextbox','Monile Phone')
             .pause(3000)
             .click('#nav-search-submit-button')
+            .pause(3000)
+        },
+        'Test2':function(browser){
+            browser.click('xpath',"//span[text()='Samsung']")
+            .pause(2000)
         }
 }
